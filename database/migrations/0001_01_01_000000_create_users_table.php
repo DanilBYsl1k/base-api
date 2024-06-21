68168af5c1b08a03f0ddfaf19fd9f13749b69347
djaman <buslik@example.com>
2024-06-21 17:56:43 +0300

    migrate table roles and make jwt controller

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->string('role')->default('user');
+            $table->foreign('role')->references('role_name')->on('roles');
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+});
+
+Route::group(["middleware" => ['auth:api']], function () {
+    Route::get('profile', [AuthController::class, 'profile'])->name('profile');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('refreshToken', [AuthController::class, 'refreshToken'])->name('refreshToken');
+    Route::put('resetPassword', [AuthController::class, 'resetPassword'])->name('resetPassword');
+});
+
+Route::fallback(function () {
+    return response()->json(['message' => 'url not found'], 404);
+});
+
