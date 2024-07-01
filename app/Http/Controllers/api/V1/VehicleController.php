eceae4fa33678fbcb16186b54e5f20e64f701765
djaman <buslik@example.com>
2024-07-01 18:54:34 +0300

    add event and migration for vehicle and images

--- app/Http/Controllers/api/V1/VehicleController.php
+++ app/Http/Controllers/api/V1/VehicleController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers\api\V1;
 
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\VehicleResource;
+use App\Models\Vehicle;
+use http\Env\Request;
 
-class VehicleController extends Controller
+class VehicleController extends BaseController
 {
-    //
+    public function index(Request $request)
+    {
+        $per_page = $request['page'];
+        $vehicles = Vehicle::orderBy('id', 'asc')
+            ->simplePaginate(20, ['*'], 'page', $per_page);
+
+        return VehicleResource::collection($vehicles);
+    }
+
+    public function create()
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\V1\VehicleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -10,10 +11,16 @@ Route::prefix('auth')->name('auth.')->group(function () {
     Route::put('reset-password', [AuthController::class, 'resetPassword'])->name('resetPassword');
 });
 
+Route::get('vehicles', [VehicleController::class, 'index'])->name('vehicles.index');
+Route::get('vehicles/{id}', [VehicleController::class, 'show'])->name('vehicles.index');
+
 Route::group(["middleware" => ['auth:api']], function () {
     Route::get('profile', [AuthController::class, 'profile'])->name('profile');
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('refresh-token', [AuthController::class, 'refresh'])->name('refresh');
-    Route::post('');
+
+    Route::group(["prefix" => "vehicles"], function () {
+        Route::post('create', [VehicleController::class, 'create'])->name('create');
+    })->name('vehicles.');
 });
 
